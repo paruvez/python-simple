fd381d6a376c7b5d717674dad5242ce73c7327cf
Parvez Alam <72540030+paruvez@users.noreply.github.com>
2025-08-23 00:12:22 +0530

    uploded

---
+++ task1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number= "))
+num2=int(input("Enter second number= "))
+Add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num2/num1
+print("Addition = ",Add)
+print("Subtraction =",sub)
+print("Multiplication = ",mul)
+print("Division = " ,div )
---
+++ task2.py
@@ -0,0 +1,3 @@
+fname=input("Enter Your First name: ")
+lname=input("Enter Your Second name: ")
+print ("hello!", fname, "", lname," Welcome to Parvez's code")
